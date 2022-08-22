0b37c713f9e91485917badd99b708e3d59ab92e2
VictorMunene <victor.munene@student.moringaschool.com>
2022-08-22 14:13:55 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(row => row.result === 'W');
+    if(typeof(win) === 'undefined') {
+        return undefined;
+    } else {
+        return win.year;
+    }
+}
